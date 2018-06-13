91b8b69dbd1a3768a81661b497046221107727fa
EtherealRhyme <EtherealCoder@163.com>
2018-06-14 03:07:59 +0800

    inject ware

--- dee-chrome-extension/js/inject.js
+++ dee-chrome-extension/js/inject.js
@@ -8,18 +8,33 @@ function sendMessageToContentScriptByPostMessage(data) {
 
 function getDeeWareMag() {
     //mod-detail-title
-    $("#mod-detail-title")
+    var title = $("#mod-detail-title").children("h1").text();
+    console.log("title: " + title);
     //mod-detail-description
+    var descJson = $("#mod-detail-description").attr("data-mod-config");
+    console.log("desc: " + descJson);
     //mod-detail-gallery
+    var imgs = [];
+    $("div.tab-content-container").find("li").each(function (index, element) {
+        console.log(index);
+        imgs[index] = $(element).attr("data-imgs");
+        console.log("banner: " + $(element).attr("data-imgs"));
+    });
+
+    var xxmsg = {
+        deetitle : title,
+        deedesc : descJson,
+        deebanner : imgs,
+        deesku : iDetailData
+    }
+
     //mod-detail-postage
     //mod-detail-purchasing
     //mod-detail-attributes
     //mod-detail-video
 
-
-
     window.postMessage({
         cmd: 'sp_msg',
-        msg: iDetailData
+        msg: xxmsg
     }, '*');
 }
